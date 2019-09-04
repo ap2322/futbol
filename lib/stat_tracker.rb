c99cdc318ec14db9b6bb6c36013298c023e3b403
Alice Post <alicep2322@gmail.com>
2019-09-03 18:34:21 -0600

    Start parse of CSVs into arrays. Next step is to make array of hashes.

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,6 +1,20 @@
 class StatTracker
 
   def self.from_csv(hash)
-    hash
+    # map each line to the first level key
+    hash.each do |key, value|
+      hash[key] = File.open(value).map do |line|
+        line = line.split("\r\n")
+      end
+    end
+
+    # arr_of_arrs = []
+    # hash.each do |key, value|
+    #   binding.pry
+    #   arr_of_arrs << CSV.read(value)
+    # end
+
   end
+
+
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -22,4 +22,13 @@ class StatTrackerTest < Minitest::Test
     assert_equal @locations, @stat_tracker
   end
 
+  def test_read_file_lines
+    line1_string = "game_id,season,type,date_time,away_team_id,home_team_id,away_goals,home_goals,venue,venue_link"
+    assert_equal [line1_string], @stat_tracker[:games][0]
+  end
+
+  # def test_map_file_to_hash
+  #   assert_equal 7441, @stat_tracker[:games].length
+  # end
+
 end
