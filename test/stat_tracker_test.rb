9d4ed522c2b1abd69838b6042675f8c12ba188a0
Alice Post <alicep2322@gmail.com>
2019-09-11 11:52:04 -0600

    Add tests for helper method and instance results from init
    Co-authored-by: David Lopez <43835133+Not-Zorro@users.noreply.github.com>
    Co-authored-by: Tyler Balogh <52803986+t-balogh@users.noreply.github.com>

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -22,16 +22,43 @@ class StatTrackerTest < Minitest::Test
     @stat_tracker = StatTracker.from_csv(@locations)
   end
 
-  def test_it_exists_and_attributes
+  def test_it_exists
     assert_instance_of StatTracker, @stat_tracker
   end
 
-  def test_attributes
+  def test_attributes_of_created_hashes
+    expected_team_keys = ["1", "4", "26", "14", "6", "3", "5", "17", "28", "16", "2", "30", "19", "24", "15", "20"]
     assert_instance_of Hash, @stat_tracker.all_teams
+    assert_equal expected_team_keys, @stat_tracker.all_teams.keys
+    assert_instance_of Team, @stat_tracker.all_teams.values.first
+    assert_equal "1", @stat_tracker.all_teams.values.first.team_id
+    assert_equal "Atlanta United", @stat_tracker.all_teams.values.first.team_name
+
     assert_instance_of Hash, @stat_tracker.all_games
-    assert_equal Team, @stat_tracker.all_teams.values.first.class
-    assert_equal Game, @stat_tracker.all_games.values.first.class
+    assert_equal 40, @stat_tracker.all_games.keys.length
+    assert_equal "2012030221", @stat_tracker.all_games.keys.first
+    assert_instance_of Game, @stat_tracker.all_games.values.first
+    assert_equal "2012030221", @stat_tracker.all_games.values.first.game_id
+    assert_equal "Postseason", @stat_tracker.all_games.values.first.type
+
     assert_equal GameTeam, @stat_tracker.all_game_teams.values[0]["home"].class
-    assert_equal GameTeam, @stat_tracker.all_game_teams.values[0]["away"].class
+    assert_equal 40, @stat_tracker.all_game_teams.keys.length
+    assert_equal "2012020087", @stat_tracker.all_game_teams.keys.first
+    assert_equal "2012020087", @stat_tracker.all_game_teams.keys.first
+    assert_instance_of Hash, @stat_tracker.all_game_teams.values.first
+    assert_equal ["away", "home"],@stat_tracker.all_game_teams.values.first.keys
+    assert_instance_of GameTeam, @stat_tracker.all_game_teams.values[0]["away"]
+    assert_equal "2012020087", @stat_tracker.all_game_teams.values.first["away"].game_id
+    assert_equal 7, @stat_tracker.all_game_teams.values.first["away"].shots
+  end
+
+  def test_generate_hash
+    test_hash = StatTracker.generate_hash('./test/test_data/teams_sample.csv', Team)
+    test_team = test_hash["1"]
+
+    assert_instance_of Hash, test_hash
+    assert_instance_of Team, test_team
+    assert_equal "Atlanta United", test_team.team_name
+    assert_equal "ATL", test_team.abbreviation
   end
 end
