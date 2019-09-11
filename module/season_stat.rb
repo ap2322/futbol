dddcbed985847f2c981dce5242aa9e4ab6e0b929
Alice Post <alicep2322@gmail.com>
2019-09-11 11:56:14 -0600

    Incomplete update
    Co-authored-by: David Lopez <43835133+Not-Zorro@users.noreply.github.com>
    Co-authored-by: Tyler Balogh <52803986+t-balogh@users.noreply.github.com>

--- module/season_stat.rb
+++ module/season_stat.rb
@@ -2,11 +2,6 @@ module SeasonStat
 
   def team_game_info(team_id)
     team_info_hash = Hash.new
-    team_info_hash["team_id"] = team_id
-    team_info_hash["franchise_id"] = all_teams[team_id].franchise_id
-    team_info_hash["team_name"] = all_teams[team_id].team_name
-    team_info_hash["abbreviation"] = all_teams[team_id].abbreviation
-    team_info_hash["link"] = all_teams[team_id].link
     team_info_hash["game_teams_objs"] = seasons_to_game_teams(team_id).values.flatten
     team_info_hash
   end
